1a2a9e1dd4d136ff0d9c601679126d97d67e19f5
vlady <jetcostar0118@gmail.com>
2024-10-10 23:43:00 +0300

    a

---
+++ src/components/contact.jsx
@@ -0,0 +1,188 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import React from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const clearState = () => setState({ ...initialState });
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+    
+    {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
+    
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .then(
+        (result) => {
+          console.log(result.text);
+          clearState();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Contact Us</h2>
+                <p>
+                  Today and one of our clinical team will respond to you right away and answer your questions.
+                </p>
+              </div>
+              <form name="sentMessage" validate onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="first_name"
+                        name="first_name"
+                        className="form-control"
+                        placeholder="First Name"
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="last_name"
+                        name="last_name"
+                        className="form-control"
+                        placeholder="Last Name"
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    id="email_address"
+                    name="email_address"
+                    className="form-control"
+                    placeholder="Email Address"
+                    required
+                    onChange={handleChange}
+                  />
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    id="phone_number"
+                    name="phone_number"
+                    className="form-control"
+                    placeholder="Phone Number"
+                    required
+                    onChange={handleChange}
+                  />
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="flex">
+                  <checkbox></checkbox>
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Message"
+                    required
+                    onChange={handleChange}
+                  ></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div id="success"></div>
+                <button type="submit" className="btn btn-custom btn-lg">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Address
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Phone
+                </span>{" "}
+                {props.data ? props.data.phone : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : "/"}>
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.linkedin : "/"}>
+                      <i className="fa fa-linkedin-square"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.instagram : "/"}>
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            Copyright © 2024 CareONE Concierge – All Rights Reserved{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
